182693dfb30ae6beb85ee7b0855b1557c274dc1d
Sp33dW33d <prayashthelegend@gmail.com>
2026-01-16 20:36:50 +0530

    added artist names to trackline

--- playlist_maker.py
+++ playlist_maker.py
@@ -1,8 +1,8 @@
 import pickle
 import requests as req
 import json
-import keys
 import re
+import keys
 
 # def error_catcher(base_fn):
 #     def enhanced_fn(*args, **kwargs):
@@ -13,7 +13,7 @@ import re
 #             return f"Error:, {response.status_code}, {response.text}"
 #     return enhanced_fn
 
-def get_token(client_id, client_secret):
+def get_token(client_id, client_secret) -> str:
     "returhs an access token"
 
     SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
@@ -30,13 +30,13 @@ def get_token(client_id, client_secret):
     response.raise_for_status()
     return response.json()["access_token"]
     
-def get_tracklist(playlist_id, access_token):
+def get_tracklist(playlist_id, access_token) -> list[tuple]:
     response = req.get(url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?limit=50&offset=0", headers = {"Authorization": f"Bearer {access_token}"})
 
     tracklist = []
     while True:
         for item in response.json()["items"]:
-            tracklist.append(item["track"]["name"])
+            tracklist.append((item["track"]["artists"][0]["name"], item["track"]["name"]))
         next = response.json()["next"]
         if next == None:
             return tracklist
@@ -44,31 +44,41 @@ def get_tracklist(playlist_id, access_token):
             response = req.get(url = next, headers = {"Authorization": f"Bearer {access_token}"})
             continue
 
-def get_playlist_name(playlist_id, access_token):
-    response = req.get(url = f"https://api.spotify.com/v1/playlists/{playlist_id}", headers = {"Authorization": f"Bearer {access_token}"})
+def get_playlistid(playlistlink) -> str:
+    return playlistlink.split("/")[-1]
 
+def get_playlist_name(playlist_id, access_token) -> str:
+    response = req.get(url = f"https://api.spotify.com/v1/playlists/{playlist_id}", headers = {"Authorization": f"Bearer {access_token}"})
     return response.json()["name"]
 
+def generate_trackline(location, track) -> str:
+    return f"{location}/{track[0]} - {track[1]}.mp3\n"
+
 def generate_playlist_file(playlist_name, tracklist):
-    location = rf"Music/Playlists/{playlist_name}"    
+    location = rf"Music/Playlists/{playlist_name}"
     playlist_file = rf"D:\Coding_Stuff\Codes\Python\playlist-maker\data\{playlist_name}.m3u"
-    # playlist_file = rf"D:\Coding_Stuff\Codes\Python\playlist-maker\data\uj_chill.m3u"
 
     with open(playlist_file, mode = "w", encoding="utf-8") as f:
         f.write("#EXTM3U\n")
         for track in tracklist:
-            f.write(f"{location} - {track}.mp3\n")
+            f.write(generate_trackline(location, track))
     
     return 1
 
 def sanitize_filename(filename: str) -> str:
     return re.sub(r'[\\/:*?"<>|]', '_', filename)
 
+# ---
+
 if __name__ == "__main__":
-    PLAYLIST_ID = "28tL7RsJGjCElo3cC8dty8"
+    link = "https://open.spotify.com/playlist/28tL7RsJGjCElo3cC8dty8"
+    PLAYLIST_ID = get_playlistid(link)
     ACCESS_TOKEN = get_token(keys.CLIENT_ID, keys.CLIENT_SECRET)
 
     tracks = get_tracklist(PLAYLIST_ID, ACCESS_TOKEN)
     playlist_name = sanitize_filename(get_playlist_name(PLAYLIST_ID, ACCESS_TOKEN))
     if generate_playlist_file(playlist_name, tracks):
         print("Playlist file generated successfully")
+    
+    # tracks = get_tracklist(PLAYLIST_ID, ACCESS_TOKEN)
+    # print(tracks)
